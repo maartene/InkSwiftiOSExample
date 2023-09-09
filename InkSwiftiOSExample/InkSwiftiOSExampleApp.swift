286a48a9913044b895c77d315b690f1734c39a1d
Maarten Engels <maartene@mac.com>
2023-09-09 20:59:45 +0200

    Initial Commit

---
+++ InkSwiftiOSExample/InkSwiftiOSExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkSwiftiOSExampleApp.swift
+//  InkSwiftiOSExample
+//
+//  Created by Maarten Engels on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InkSwiftiOSExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
